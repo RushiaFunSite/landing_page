15bad53cc449976c76218ecc8c113ce17c0c20c5
Riml <38300385+RimlTempest@users.noreply.github.com>
2021-09-17 02:51:18 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 
-const Home: NextPage = () =>{
-  console.log("test");
+const Home: NextPage = ()=>{
+  console.log("test")
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
