01b402be6bbffcc07658549e9cc4556ba593de26
RimlTempest <riml.slime@gmail.com>
2021-09-17 01:11:47 +0900

    first commit

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  roots: ['<rootDir>/'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/(.+)': '<rootDir>/$1',
+  },
+};
+
+export default config;
