4ba25bbb02011807786707648f2f029df9eab34d
RimlTempest <riml.slime@gmail.com>
2021-10-01 23:17:24 +0900

    update: backgroundImageだと表示される？

--- src/components/molecules/headerItems/headerTitle.tsx
+++ src/components/molecules/headerItems/headerTitle.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
 // import Image from 'next/image';
+import styles from './headertitle.module.scss';
 
 const HeaderTitle = () => {
   return (
     <div className="flex justify-center items-center lg:text-2xl">
-      <img alt="rushia_butterfly" src="/butterfly.svg" height="60px" width="70px" />
+      <div className={styles.titleImage}></div>
       <p>
         潤羽るしあ<span className="text-green-500">ファンサイト</span>
       </p>
