1535566ef1fcca96771d2626dbbfdb9e9ab0bff9
RimlTempest <riml.slime@gmail.com>
2021-10-02 06:39:36 +0900

    update: スクロール制御処理追加 #61

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
-import { Button } from '@/components/Button';
-import SNSButtonGroup from '@/components/molecules/snsbuttongroup/SNSButtonGroup';
 import Layout from '@/layout/layout';
 import type { NextPage } from 'next';
 import * as gtag from '@/lib/gtag';
+import React, { useEffect, useState } from 'react';
+import MainSection from '@/components/templates/MainSection';
+import AboutSection from '@/components/templates/AboutSection';
+import GoodsSection from '@/components/templates/GoodsSection';
+import FAQSection from '@/components/templates/FAQSection';
+import Header from '@/components/organisms/header/Header';
+import useScroll from '@/hooks/useScroll';
 
 const Home: NextPage = () => {
-  console.log('test');
+  const { isHeaderActive } = useScroll(900);
   const handleClicked = (e: { preventDefault: () => void }) => {
     e.preventDefault();
 
@@ -17,63 +22,15 @@ const Home: NextPage = () => {
   };
   return (
     <Layout>
-      <div className="px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-        {/* <SNSButtonGroup label="SNS" colorMode="#77d2b8" /> */}
-        <Button label="test" primary={true} onClick={() => handleClicked} />
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">pages/index.js</code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="sticky top-0 z-50">
+        <div className="flex flex-col w-screen">
+          <Header isActive={isHeaderActive} />
         </div>
       </div>
+      <MainSection />
+      <AboutSection />
+      <GoodsSection />
+      <FAQSection />
     </Layout>
   );
 };
