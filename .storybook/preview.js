001afa864a02dd1198c846fa9ba805d445745a6c
RimlTempest <riml.slime@gmail.com>
2021-10-01 18:09:46 +0900

    fix: storybookでnext/imageが読み込まれないのを修正

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import '../src/styles/globals.scss';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import * as nextImage from 'next/image';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -13,3 +14,8 @@ export const parameters = {
     viewports: INITIAL_VIEWPORTS,
   },
 };
+// Replace next/image for Storybook
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: (props) => <img {...props} />,
+});
