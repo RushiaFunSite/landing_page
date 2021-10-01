2744d8fb73af8c57c1b1d50ccf9638a22087d5e8
RimlTempest <riml.slime@gmail.com>
2021-10-01 18:08:26 +0900

    add: storybook

--- src/stories/templates/MainSection.stories.tsx
+++ src/stories/templates/MainSection.stories.tsx
@@ -1,1 +1,11 @@
-export {};
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import MainSection from '../../components/templates/MainSection';
+
+export default {
+  title: 'Template/MainSection',
+  component: MainSection,
+} as ComponentMeta<typeof MainSection>;
+
+export const Template: ComponentStory<typeof MainSection> = () => <MainSection />;
