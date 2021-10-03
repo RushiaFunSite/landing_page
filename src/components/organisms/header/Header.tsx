d561fa97c4f8487ef1637191f6d7c46871c4e44b
RimlTempest <riml.slime@gmail.com>
2021-10-04 08:10:35 +0900

    update: useFetchHeaderItemから値を取得し適用するようにした

--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -11,49 +11,7 @@ import { IconType } from 'react-icons';
 import HeaderLinkLabel from '../../atoms/linkLabels/headerLinkLabel';
 import HeaderTitle from '../../molecules/headerItems/headerTitle';
 import styles from './header.module.scss';
-
-type Service = {
-  name: string;
-  description: string;
-  href: string;
-  icon: IconType;
-};
-
-type MenuAction = {
-  name: string;
-  href: string;
-  icon: IconType;
-};
-
-const services: Service[] = [
-  {
-    name: 'クイズ',
-    description: '潤羽るしあちゃんについてのクイズです。',
-    href: '/',
-    icon: FaQuestion,
-  },
-  {
-    name: 'るしあちゃんボタン',
-    description: 'るしあちゃんボタンです。',
-    href: '/',
-    icon: GiPlayButton,
-  },
-  {
-    name: 'るしあちゃんなでなで',
-    description: 'るしあちゃん不足でなでなでしたいときに撫でられるサービスです。',
-    href: '/',
-    icon: RiHandHeartLine,
-  },
-];
-
-const menuAction: MenuAction[] = [
-  {
-    name: 'Youtube',
-    href: 'https://www.youtube.com/channel/UCl_gCybOJRIgOXw6Qb4qJzQ',
-    icon: FaYoutube,
-  },
-  { name: 'Twitter', href: 'https://twitter.com/uruharushia', icon: VscTwitter },
-];
+import useFetchHeaderItem from '@/hooks/useFetchHeaderItem';
 
 function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(' ');
@@ -64,6 +22,30 @@ type headerProps = {
 };
 
 const Header: VFC<headerProps> = (props) => {
+  const { data } = useFetchHeaderItem();
+  const headerItems = data?.headerData.headerItems;
+  // service
+  const service = data?.headerData.services;
+  const serviceIcon = [{ icon: FaQuestion }, { icon: GiPlayButton }, { icon: RiHandHeartLine }];
+  const serviceItems: {
+    icon: IconType;
+    serviceName: string;
+    description: string;
+    href: string;
+    isRunning: boolean;
+  }[] = [];
+  service?.[0].serviceItems?.map((item, index) => {
+    serviceItems.push({ ...item, ...serviceIcon[index] });
+  });
+
+  // menuAction
+  const menuActions = data?.headerData.menuActions;
+  const menuActionIcon = [{ icon: FaYoutube }, { icon: VscTwitter }];
+  const menuItems: { icon: IconType; actionName: string; href: string }[] = [];
+  menuActions?.map((item, index) => {
+    menuItems.push({ ...item, ...menuActionIcon[index] });
+  });
+
   const style = props.isActive ? styles.active : styles.notActive;
 
   return (
@@ -81,7 +63,7 @@ const Header: VFC<headerProps> = (props) => {
                   </Link>
                 </div>
                 <div className="-mr-2 -my-2 md:hidden">
-                  <Popover.Button className="bg-transparent rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <Popover.Button className="bg-transparent rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-400">
                     <span className="sr-only">Open menu</span>
                     <VscMenu className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
@@ -96,7 +78,7 @@ const Header: VFC<headerProps> = (props) => {
                             'group bg-transparent rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400',
                           )}
                         >
-                          <span>サービス</span>
+                          <span>{service?.[0].serviceLabel}</span>
                           <VscChevronDown
                             className={classNames(
                               open ? 'text-gray-600' : 'text-gray-400',
@@ -121,35 +103,39 @@ const Header: VFC<headerProps> = (props) => {
                           >
                             <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                               <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                                {services.map((item) => (
-                                  <Link key={item.name} href={item.href}>
-                                    <a className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                                      <item.icon
-                                        className="flex-shrink-0 h-6 w-6 text-green-400"
-                                        aria-hidden="true"
-                                      />
-                                      <div className="ml-4">
-                                        <p className="text-base font-medium text-gray-900">
-                                          {item.name}
-                                        </p>
-                                        <p className="mt-1 text-sm text-gray-500">
-                                          {item.description}
-                                        </p>
-                                      </div>
-                                    </a>
-                                  </Link>
-                                ))}
+                                {serviceItems.map((item) =>
+                                  item.isRunning ? (
+                                    <Link key={item.serviceName} href={item.href}>
+                                      <a className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                                        <item.icon
+                                          className="flex-shrink-0 h-6 w-6 text-green-400"
+                                          aria-hidden="true"
+                                        />
+                                        <div className="ml-4">
+                                          <p className="text-base font-medium text-gray-900">
+                                            {item.serviceName}
+                                          </p>
+                                          <p className="mt-1 text-sm text-gray-500">
+                                            {item.description}
+                                          </p>
+                                        </div>
+                                      </a>
+                                    </Link>
+                                  ) : (
+                                    ''
+                                  ),
+                                )}
                               </div>
                               <div className="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
-                                {menuAction.map((item) => (
-                                  <div key={item.name} className="flow-root">
-                                    <Link key={item.name} href={item.href}>
+                                {menuItems.map((item) => (
+                                  <div key={item.actionName} className="flow-root">
+                                    <Link key={item.actionName} href={item.href}>
                                       <a className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100">
                                         <item.icon
                                           className="flex-shrink-0 h-6 w-6 text-green-400"
                                           aria-hidden="true"
                                         />
-                                        <span className="ml-3">{item.name}</span>
+                                        <span className="ml-3">{item.actionName}</span>
                                       </a>
                                     </Link>
                                   </div>
@@ -161,9 +147,15 @@ const Header: VFC<headerProps> = (props) => {
                       </>
                     )}
                   </Popover>
-                  <HeaderLinkLabel href="#AboutSection" label="潤羽るしあとは" />
-                  <HeaderLinkLabel href="#GoodsSection" label="グッズ紹介" />
-                  <HeaderLinkLabel href="#FAQSection" label="Q & A" />
+                  {headerItems?.map((item) => {
+                    return (
+                      <HeaderLinkLabel
+                        key={item.headerLabel}
+                        href={item.href}
+                        label={item.headerLabel}
+                      />
+                    );
+                  })}
                 </Popover.Group>
               </div>
             </div>
@@ -187,7 +179,7 @@ const Header: VFC<headerProps> = (props) => {
                     <div className="flex items-center justify-between">
                       <HeaderTitle />
                       <div className="-mr-2">
-                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-400">
                           <span className="sr-only">Close menu</span>
                           <VscClose className="h-6 w-6" aria-hidden="true" />
                         </Popover.Button>
@@ -195,27 +187,37 @@ const Header: VFC<headerProps> = (props) => {
                     </div>
                     <div className="mt-6">
                       <nav className="grid gap-y-8">
-                        {services.map((item) => (
-                          <Link key={item.name} href={item.href}>
-                            <a className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-                              <item.icon
-                                className="flex-shrink-0 h-6 w-6 text-green-400"
-                                aria-hidden="true"
-                              />
-                              <span className="ml-3 text-base font-medium text-gray-900">
-                                {item.name}
-                              </span>
-                            </a>
-                          </Link>
-                        ))}
+                        {serviceItems.map((item) =>
+                          item.isRunning ? (
+                            <Link key={item.serviceName} href={item.href}>
+                              <a className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                                <item.icon
+                                  className="flex-shrink-0 h-6 w-6 text-green-400"
+                                  aria-hidden="true"
+                                />
+                                <span className="ml-3 text-base font-medium text-gray-900">
+                                  {item.serviceName}
+                                </span>
+                              </a>
+                            </Link>
+                          ) : (
+                            ''
+                          ),
+                        )}
                       </nav>
                     </div>
                   </div>
                   <div className="py-6 px-5 space-y-6">
                     <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                      <HeaderLinkLabel href="#AboutSection" label="潤羽るしあとは" />
-                      <HeaderLinkLabel href="#GoodsSection" label="グッズ紹介" />
-                      <HeaderLinkLabel href="#FAQSection" label="Q & A" />
+                      {headerItems?.map((item) => {
+                        return (
+                          <HeaderLinkLabel
+                            key={item.headerLabel}
+                            href={item.href}
+                            label={item.headerLabel}
+                          />
+                        );
+                      })}
                     </div>
                   </div>
                 </div>
