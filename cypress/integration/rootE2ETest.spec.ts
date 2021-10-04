a7e71716cd322f6a945ef9eb4eca717ffe70a3b6
RimlTempest <riml.slime@gmail.com>
2021-10-04 22:21:33 +0900

    update: FAQセクションのe2eテスト追加

--- cypress/integration/rootE2ETest.spec.ts
+++ cypress/integration/rootE2ETest.spec.ts
@@ -11,3 +11,12 @@ it('グッズセクションに内容が含まれているか', () => {
   cy.contains('Booth');
   cy.contains('ホロライブ公式ショップ');
 });
+it('FAQセクションに内容が含まれているか', () => {
+  cy.contains('よくあるご質問（FAQ:Frequently Asked Question）');
+  cy.contains('待機中のルール');
+  cy.contains('鳩とは？？');
+  cy.contains('メンバーとは、どうやって加入するのか?');
+  cy.contains('指示厨/ワザップとは？');
+  cy.contains('スパチャとは？');
+  cy.contains('るしあちゃんかわいい');
+});
