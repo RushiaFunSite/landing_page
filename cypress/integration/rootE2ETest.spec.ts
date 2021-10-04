d5459bdf61f97de7043bff70c283ab034d372049
RimlTempest <riml.slime@gmail.com>
2021-10-04 20:53:12 +0900

    update: グッズセクションのe2eテスト

--- cypress/integration/rootE2ETest.spec.ts
+++ cypress/integration/rootE2ETest.spec.ts
@@ -5,3 +5,9 @@ it('メインセクションに内容が含まれているか', () => {
   cy.contains('UruhaRushia');
   cy.contains('fanSite!');
 });
+it('グッズセクションに内容が含まれているか', () => {
+  cy.contains('グッズ紹介');
+  cy.contains('公式');
+  cy.contains('Booth');
+  cy.contains('ホロライブ公式ショップ');
+});
