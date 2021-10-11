37c1a21539dfe5a95d9a3bd59f1160de298b3f75
RimlTempest <riml.slime@gmail.com>
2021-10-12 04:20:03 +0900

    update: snsシェアでfacebookを実装したのでseoでも実装 #111

--- src/components/wrapperComponent/SEO.tsx
+++ src/components/wrapperComponent/SEO.tsx
@@ -1,42 +1,45 @@
 import { NextSeo } from 'next-seo';
 
 const SEO = () => {
-  return (
-    <NextSeo
-      title="潤羽るしあファンサイト"
-      description="潤羽るしあを広めるために作られたファンサイト"
-      canonical="https://www.uruharushia.work/"
-      openGraph={{
-        url: 'https://www.uruharushia.work',
-        title: '潤羽るしあファンサイト',
-        description: '潤羽るしあを広めるために作られたファンサイト',
-        images: [
-          {
-            url: 'https://www.uruharushia.work/maskable_icon_x384.png',
-            width: 384,
-            height: 384,
-            alt: '潤羽るしあファンサイト',
-            type: 'image/png',
-          },
-          {
-            url: 'https://www.uruharushia.work/maskable_icon_x512.png',
-            width: 512,
-            height: 512,
-            alt: '潤羽るしあファンサイト',
-            type: 'image/png',
-          },
-          { url: 'https://www.uruharushia.work/maskable_icon_x512.png' },
-          { url: 'https://www.uruharushia.work/maskable_icon_x512.png' },
-        ],
-        site_name: '潤羽るしあファンサイト',
-      }}
-      twitter={{
-        handle: '@Fande4d',
-        site: '@Fande4d',
-        cardType: 'summary_large_image',
-      }}
-    />
-  );
+    return (
+        <NextSeo
+            title="潤羽るしあファンサイト"
+            description="潤羽るしあを広めるために作られたファンサイト"
+            canonical="https://www.uruharushia.work/"
+            openGraph={{
+                url: 'https://www.uruharushia.work',
+                title: '潤羽るしあファンサイト',
+                description: '潤羽るしあを広めるために作られたファンサイト',
+                images: [
+                    {
+                        url: 'https://www.uruharushia.work/maskable_icon_x384.png',
+                        width: 384,
+                        height: 384,
+                        alt: '潤羽るしあファンサイト',
+                        type: 'image/png',
+                    },
+                    {
+                        url: 'https://www.uruharushia.work/maskable_icon_x512.png',
+                        width: 512,
+                        height: 512,
+                        alt: '潤羽るしあファンサイト',
+                        type: 'image/png',
+                    },
+                    { url: 'https://www.uruharushia.work/maskable_icon_x512.png' },
+                    { url: 'https://www.uruharushia.work/maskable_icon_x512.png' },
+                ],
+                site_name: '潤羽るしあファンサイト',
+            }}
+            twitter={{
+                handle: '@Fande4d',
+                site: '@Fande4d',
+                cardType: 'summary_large_image',
+            }}
+            facebook={{
+                appId: '171186011849340',
+            }}
+        />
+    );
 };
 
 export default SEO;
